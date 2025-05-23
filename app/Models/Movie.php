799cfdc54eb3e50ec8a90d3a12bb09635f195793
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-05-23 12:21:38 +0200

    Ajout de HasFactory

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['title', 'commentaire', 'note', 'annee'];
 }
