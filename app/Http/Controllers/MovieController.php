a781c8eb2f9819ff7aef20f6ca1b2bb340f5cae8
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-05-23 11:17:24 +0200

    Implémentation des méthodes CRUD dans le controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies= Movie::all();
+        return view('movies.index', compact('movies'));
+    }
+
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'commentaire' => 'required|string',
+            'note' => 'required|float',
+            'annee' => 'required|date',
+
+        ]);
+
+        Movie::create($request->all());
+        return redirect()->route('movies.index')->with('success', 'Movie created successfully.');
+    }
+
+    public function edit(Movie $movie)
+    {
+        return view('movies.edit', compact('movie'));
+    }
+
+    public function update(Request $request, Movie $movie)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'commentaire' => 'required|string',
+            'note' => 'required|float',
+            'annee' => 'required|date',
+        ]);
+
+        $movie->update($request->all());
+        return redirect()->route('movies.index')->with('success', 'Movie updated successfully.');
+    }
+
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+        return redirect()->route('movies.index')->with('success', 'Movie deleted successfully.');
+    }
 }
